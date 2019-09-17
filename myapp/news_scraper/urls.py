073d8c1949f0dcc9d5096d99f48e04866425013a
Lucas Cheon <inwoo@fb.com>
2019-09-17 11:59:24 +0900

    main page view template

--- myapp/news_scraper/urls.py
+++ myapp/news_scraper/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/keywords/', views.keywords, name='keywords'),
 ]
--- myapp/news_scraper/views.py
+++ myapp/news_scraper/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")# -*- coding: utf-8 -*-
+    latest_article_list = Article.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_article_list': latest_article_list,
+    }
+    return render(request, 'news_scraper/index.html', context)
+
+def detail(request, article_id):
+    return HttpResponse("You're looking at article %s." % article_id)
+
+def keywords(request, article_id):
+    response = "You're looking at the keywords of article %s."
+    return HttpResponse(response % article_id)
